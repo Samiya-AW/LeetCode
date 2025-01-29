ce88da27a9cd8f7a3cfa2d7fbe248ca1734b257e
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-01-29 17:35:52 +0500

    Time: 12 ms (40.24%), Space: 34.9 MB (14.3%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,15 +1,26 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        # Hash Table Solution
+        # Hash Table Solution 2
         hsh = {}
 
-        for i in nums:
-            hsh[i] = 1 + hsh.get(i, 0)
-            if hsh[i] == 2:
+        for num in nums:
+            if num not in hsh:
+                hsh[num] = 1
+            else:
                 return True
-            
+
         return False
+
+        #  Hash Table Solution 1
+        # hsh = {}
+
+        # for i in nums:
+        #     hsh[i] = 1 + hsh.get(i, 0)
+        #     if hsh[i] == 2:
+        #         return True
+            
+        # return False
 
         # # Brute Force Solution
         # nums = sorted(nums)
