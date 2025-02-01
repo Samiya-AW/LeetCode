01b9592257f8f5b451def1c0cde96243dd06deb4
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-02-01 16:47:02 +0500

    Time: 1151 ms (15.8%), Space: 18.7 MB (42.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        
+        nums.sort()
         for i in range(0, n + 1):
             if i not in nums:
                 return i
