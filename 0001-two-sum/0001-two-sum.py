47bce852c2a6ab3d7e18c620f00dd86df77d438f
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-02-07 13:28:29 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            j = i + 1
+            if nums[i] + nums[j] == target:
+                return i, j
         # for i in range(len(nums)):
         #     for j in range(i + 1, len(nums)):
         #         if nums[i] + nums[j] == target:
@@ -10,7 +14,7 @@ class Solution:
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
