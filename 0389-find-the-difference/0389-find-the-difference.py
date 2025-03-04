f3595db1e533f160ba46156186bd546a597d7c4c
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-03-04 09:40:38 +0500

    Time: 0 ms (100%), Space: 17.9 MB (24.84%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,18 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        res = {}
 
-        for i in t:
-            res[i] = res.get(i, 0) + 1
+        for char in t:
+            count_s = s.count(char)
+            count_t = t.count(char)
+            if count_s != count_t:
+                return char
 
-        for j in res:
-            if res[j] != s.count(j):
-                return j
+        # Solution using Hash Table
+        # res = {}
+
+        # for i in t:
+        #     res[i] = res.get(i, 0) + 1
+
+        # for j in res:
+        #     if res[j] != s.count(j):
+        #         return j
