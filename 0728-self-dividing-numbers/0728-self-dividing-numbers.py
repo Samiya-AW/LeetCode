3c8c1c1551b12800cac8c4adaa2865f4f16fcddc
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-03-18 15:31:32 +0500

    Time: 11 ms (47.99%), Space: 18 MB (30.58%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -10,15 +10,15 @@ class Solution:
                 num_list.append(n % 10)
                 n //= 10
             
-            count = 0
+            divide_count = 0
 
             if 0 not in num_list:
 
                 for d in num_list:
                     if num % d == 0:
-                        count += 1
+                        divide_count += 1
                 
-                if count == len(num_list):
+                if divide_count == len(num_list):
                     self_dividing.append(num)
         
         return self_dividing
