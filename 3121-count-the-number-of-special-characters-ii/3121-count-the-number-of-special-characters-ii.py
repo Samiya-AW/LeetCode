9bab3aa9161445f88a2b35f880fe126e415d6ca4
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-04-02 16:12:03 +0500

    Time: 52 ms (98.56%), Space: 20.1 MB (15.85%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -5,8 +5,7 @@ class Solution:
         for c in range(0, 26, 1):
             upper = chr(c + 65)
             lower = chr(c + 97)
-            if upper in word and lower in word:
-                if word.find(lower) < word.find(upper) and word.rfind(lower) < word.find(upper):
-                    special_count += 1
+            if upper in word and lower in word and word.find(lower) < word.find(upper) and word.rfind(lower) < word.find(upper):
+                special_count += 1
         
         return special_count
