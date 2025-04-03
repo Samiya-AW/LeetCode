1b934399fdad9095ed7178138cbf6e35fefafd83
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-04-03 15:03:15 +0500

    Time: 0 ms (100%), Space: 18.5 MB (5.03%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -6,9 +6,9 @@ class Solution:
         for i in range(1, n + 1):
             if i % 3 == 0 and i % 5 == 0:
                 answer.append('FizzBuzz')
-            elif i % 3 == 0 and i % 5 != 0:
+            elif i % 3 == 0:
                 answer.append('Fizz')
-            elif i % 3 != 0 and i % 5 == 0:
+            elif i % 5 == 0:
                 answer.append('Buzz')
             else:
                 answer.append(str(i))
