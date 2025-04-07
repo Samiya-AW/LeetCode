a979dc466ab2bc7295ab68ec49105f021fcdff3e
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-04-07 07:36:54 +0500

    Time: 18 ms (26.3%), Space: 18.6 MB (14.59%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -1,12 +1,29 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
-        s = s + ' '
+
+        # Solution without Slicing
+
         res = ''
-        start = 0
-        for c in range(len(s)):
-            if s[c] == ' ':
-                res += s[start : c][: : -1] + ' '
-                start = c + 1
+        word = ''
+
+        for c in s:
+            if c != ' ':
+                word = c + word
+            else:
+                res += word + ' '
+                word = ''
+        res += word
+        return res
+
+
+        # Solution using Slicing
+
+        # s = s + ' '
+        # res = ''
+        # start = 0
+        # for c in range(len(s)):
+        #     if s[c] == ' ':
+        #         res += s[start : c][: : -1] + ' '
+        #         start = c + 1
         
-        return res.rstrip()
+        # return res.rstrip()
