5b37f4f8cfe68476eeac31f22f3156c22a90756c
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-04-15 15:16:59 +0500

    Time: 13 ms (46.69%), Space: 24.3 MB (46.69%) - LeetHub

---
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -0,0 +1,13 @@
+# from collections import defaultdict
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        pairs_count = defaultdict(int)
+        res = 0
+
+        for a, b in dominoes:
+            key = tuple(sorted((a, b)))
+            res += pairs_count[key]
+            pairs_count[key] += 1
+        
+        return res
