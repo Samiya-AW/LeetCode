cde727f12856bdeab0341d6264fa4f79d7ec6d21
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-04-30 13:27:54 +0500

    Time: 7 ms (50.16%), Space: 17.7 MB (77.74%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -2,6 +2,7 @@ class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         
         w = 1
+
         while w < len(words):
             if ''.join(sorted(words[w - 1])) == ''.join(sorted(words[w])):
                 words.pop(w)
