c6ee789bfb45fa7f2bd182ecd78dbebfb20d3580
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-05-23 18:20:40 +0500

    Time: 244 ms (15.61%), Space: 18.2 MB (21.66%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -3,14 +3,12 @@ class Solution:
         n1 = []
         n2 = []
 
-        for i in nums1:
-            if i not in n1:
-                if i not in nums2:
-                    n1.append(i)
+        for i in set(nums1):
+            if i not in nums2:
+                n1.append(i)
 
-        for j in nums2:
-            if j not in n2:
-                if j not in nums1:
-                    n2.append(j)
+        for j in set(nums2):
+            if j not in nums1:
+                n2.append(j)
     
         return [n1, n2]
