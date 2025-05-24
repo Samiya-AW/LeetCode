57012073e32fa9af54c49bff1f8013fc6fe3d1d8
Samiya-AW <samiyaabdulwahid@gmail.com>
2025-05-24 18:41:45 +0500

    Time: 19 ms (19.21%), Space: 42.7 MB (11.33%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -2,8 +2,8 @@ class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         
         common_integers = set(nums1) & set(nums2)
-
+        
         if common_integers:
             return min(common_integers)
-    
-        return -1
+        else:
+            return -1
